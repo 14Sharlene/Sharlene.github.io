59cb779b6c49ac89f4dbbf012c2debe5911ee940
14Sharlene <sa.ryan@live.com>
2019-01-18 15:14:23 +1300

    data entery

---
+++ tech/sketch.js
@@ -0,0 +1,33 @@
+//DATA types
+name=('Sharlene')  // String
+age = 48           //number INT :1,2,3,   FLOAT:2.44,5.66,6.66
+is a awesome mother = True			//bool: True,false
+children=["Tasman","Lareesah","Darian"]		//array:collection of other data type
+
+console.log(Children[0])
+//BUILDING AN OBJECT
+Sharlene{
+name:name,
+age:age,
+IsAnAwsomeMother:IsAnAwsomeMother,
+children:children,
+}
+console.log(sharlene.children);
+
+//function
+function printHello(){
+console.log("Hello");
+}
+function SayHelloCool(person){
+console.log("Hello cool" + person.name");
+}else{
+	console.log(".......")
+}
+
+function setup(test run) {
+	createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+
+}
